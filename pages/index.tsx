a6bfd61c6008152dc349f9136f7db3f01f96b3ca
Federico Zota <federico.zota@itismajo.onmicrosoft.com>
2022-09-18 22:25:40 +0200

    Adding some stuff

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import ResponsiveAppBar from '../components/appbar'
+
+const Home: NextPage = () => {
+	return (
+		<div>
+			<ResponsiveAppBar />
+		</div>
+	)
+}
+
+export default Home
